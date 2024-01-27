91cb90ce4c23581166888bbf4f73943d95697bca
kikuti <kikuti07012003@outlook.jp>
2024-01-27 13:11:48 +0900

    修正

--- Linebot-main/index.js
+++ Linebot-main/index.js
@@ -946,7 +946,7 @@ async function handleEvent(event) {
                     const parkURL = 'https://www.city.koriyama.lg.jp/soshiki/132/3252.html';  // 実際のURLに置き換えてください
     
                     // 観光地の位置情報
-                    const mapLocationText = 'Location of the 開成山公園:\nhttps://maps.google.com/?q= 37.39817472246648, 140.35831603983428';
+                    const mapLocationText = ' 開成山公園:https://maps.google.com/?q= 37.39817472246648, 140.35831603983428';
                     // 実際の緯度と経度に置き換えてください
     
                     currentState.step = 31; // 新しい質問のステップ
@@ -1133,7 +1133,7 @@ async function handleEvent(event) {
                                     const parkURL = 'https://miharukoma.com/experience/183/';  // 実際のURLに置き換えてください
                     
                                     // 観光地の位置情報
-                                    const mapLocationText = 'Location of the 三春滝桜:\nhttps://maps.google.com/?q=  37.40788850628246, 140.50014756867017';
+                                    const mapLocationText = '三春滝桜:\nhttps://maps.google.com/?q=  37.40788850628246, 140.50014756867017';
                                     // 実際の緯度と経度に置き換えてください
                     
                                     currentState.step = 31; // 新しい質問のステップ
